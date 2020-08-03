c4fe46deb6dfd416d9a2d4e6a3ca6abe86a3d50f
Elizabeth Yeoh-Wang <eyw410@gmail.com>
2020-08-03 12:10:25 -0400

    made autoRun a variable toggle-able by modal

---
+++ src/Settings.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let isAuto;
+  export let toggleAuto = () => {};
+</script>
+
+<style>
+  div {
+    text-align: center;
+  }
+</style>
+
+<div on:click={toggleAuto}>runs automatically: {isAuto}</div>
