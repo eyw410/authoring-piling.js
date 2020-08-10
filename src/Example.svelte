758bae110ce6fd1638a0a0ec7602323a46225be2
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-08-10 17:29:45 -0400

    Add example modal + loading example from gist

---
+++ src/Example.svelte
@@ -0,0 +1,137 @@
+<script>
+  import { getContext } from 'svelte';
+  import Card, { PrimaryAction, Media, MediaContent } from '@smui/card';
+  import clone from 'just-clone';
+
+  import { components } from './stores';
+
+  import { DEFAULT_COMPONENTS_NAMED } from './constants';
+
+  export let url = '';
+  export let width = '10rem';
+
+  const { close } = getContext('simple-modal');
+
+  let title = 'Loading...';
+  let thumbnail = '';
+  let fileContent = {};
+
+  const contentParser = ({ output, filename, content }) => output[filename] = content;
+
+  const fileParsers = {
+    '_pilingjs.json': ({ content }) => {
+      try {
+        const metadata = JSON.parse(content);
+        title = metadata.title || title;
+        thumbnail = metadata.thumbnail || thumbnail;
+      } catch (e) {
+        console.error('Could not parse _pilingjs.json')
+      }
+    },
+    'aggregators.js': contentParser,
+    'data.js': contentParser,
+    'data.json': contentParser,
+    'renderers.js': contentParser,
+    'style.json': contentParser,
+  }
+
+  function parseFiles(files) {
+    fileContent = Object.values(files).reduce((output, { filename, content }) => {
+      if (fileParsers[filename]) {
+        fileParsers[filename]({ output, filename, content });
+      }
+      return output;
+    }, {});
+  }
+
+  function loadExample() {
+    components.update((_components) => {
+      _components = [];
+
+      Object.entries(DEFAULT_COMPONENTS_NAMED).forEach(([name, component]) => {
+        const _component = clone(component);
+
+        if (fileContent[name]) {
+          _component.source = fileContent[name];
+        }
+
+        _components.push(_component);
+      });
+
+      return _components;
+    });
+    // Needs to trigger re-run
+    close();
+  }
+
+  $: if (url) {
+    fetch(`https://api.github.com/gists/${url}`)
+      .then(async (response) => {
+        const body = await response.json();
+        return { body, status: response.status }
+      })
+      .then(({ body, status }) => {
+        if (status !== 200) {
+          console.warn('Request unsuccessful', body.message);
+        } else {
+          title = body.description;
+          parseFiles(body.files);
+        }
+      })
+      .catch((error) => {
+        console.warn('Request failed', error);
+      });
+  }
+</script>
+
+<style>
+  .example :global(.mdc-card) {
+    position: relative;
+    box-shadow: none;
+  }
+
+  .example :global(.mdc-card:before) {
+    content: '';
+    position: absolute;
+    z-index: 1;
+    display: block;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
+    border-radius: 4px;
+    pointer-events: none;
+  }
+
+  .example-title {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    padding: 2rem 1rem 1rem 1rem;
+    color: white;
+    background: linear-gradient(0deg, rgba(0,0,0,0.8) 0%, rgba(0,0,0,0) 100%);
+  }
+
+  .example-title :global(.mdc-typography--headline6) {
+    margin: 0;
+  }
+</style>
+
+<div class="example">
+  <Card style={`width: ${width}`}>
+    <PrimaryAction on:click={loadExample}>
+      <Media
+        style={`background-image: url(${thumbnail});`}
+        aspectRatio="square"
+      >
+        <MediaContent>
+          <div class="example-title">
+            <h2 class="mdc-typography--headline6">{ title }</h2>
+          </div>
+        </MediaContent>
+      </Media>
+    </PrimaryAction>
+  </Card>
+</div>
---
+++ src/Examples.svelte
@@ -0,0 +1,117 @@
+<script>
+  import Example from './Example.svelte';
+  const booksUrl = '73dd0de929033ddadb0313fa46257835';
+</script>
+
+<style>
+  ol,
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+
+  ol {
+    margin: 1rem 0;
+  }
+
+  ul {
+    display: grid;
+    grid-gap: 1rem;
+    grid-template-columns: repeat(4, 1fr);
+  }
+</style>
+
+<ol>
+  <li>
+    <h2 class="mdc-typography--headline6">Images / Photos</h2>
+    <ul>
+      <li>
+        <Example url={booksUrl} />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h2 class="mdc-typography--headline6">Matrices / Heatmaps</h2>
+    <ul>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h2 class="mdc-typography--headline6">Scatterplots</h2>
+    <ul>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+    </ul>
+  </li>
+  <li>
+    <h2 class="mdc-typography--headline6">Line-Area Charts</h2>
+    <ul>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+      <li>
+        <Example url='' />
+      </li>
+    </ul>
+  </li>
+</ol>
